b66ff4545a987e81f50557b5ae5ecbc0212c55c8
Aishatu Ahmed <65121237+aishatuahmed@users.noreply.github.com>
2020-05-10 20:50:57 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,21 @@
+
+
+  function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    }
+
+    
+
+  function myFunction3() {
+    var z = document.getElementById("films");
+    if (z.style.display === "none") {
+        z.style.display = "block";
+    } else {
+        z.style.display = "none";
+    }
+    }
